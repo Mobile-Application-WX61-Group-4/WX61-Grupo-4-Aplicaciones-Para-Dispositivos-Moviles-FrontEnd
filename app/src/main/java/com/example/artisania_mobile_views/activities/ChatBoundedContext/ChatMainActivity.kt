7eb0b3ef1cbfa367627baba41d21d6eb7e2da44a
unknown <u202113640@upc.edu.pe>
2024-10-24 12:31:11 -0500

    fix: arreglo de flujo

--- app/src/main/java/com/example/artisania_mobile_views/activities/ChatBoundedContext/ChatMainActivity.kt
+++ app/src/main/java/com/example/artisania_mobile_views/activities/ChatBoundedContext/ChatMainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.artisania_mobile_views.activities.ChatBoundedContext
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.artisania_mobile_views.R
+import com.example.artisania_mobile_views.activities.MainMenuActivity
 
 class ChatMainActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
@@ -18,6 +21,18 @@ class ChatMainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val continueButton: Button = findViewById(R.id.continue_button)
+        val cancelButton: Button = findViewById(R.id.cancel_button)
+
+        continueButton.setOnClickListener {
+            val intent = Intent(this, ChatArtesano::class.java)
+            startActivity(intent)
+        }
+
+        cancelButton.setOnClickListener {
+            val intent = Intent(this, MainMenuActivity::class.java)
+            startActivity(intent)
         }
     }
 }
